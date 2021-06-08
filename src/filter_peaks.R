7d1640f7e11f3c459aa4b790888140bed8af4f85
ShamerD <shomax31@gmail.com>
2021-06-08 17:51:34 +0300

    fix unsaved script

--- src/filter_peaks.R
+++ src/filter_peaks.R
@@ -2,8 +2,8 @@ source('lib.R')
 
 ###
 
-NAME <- 'H3K4me3_A549.ENCFF573MUH.hg19'
-#NAME <- 'H3K4me3_A549.ENCFF832EOL.hg19'
+NAME <- 'H3K9me3_HCT116.ENCFF158YTR.hg19'
+#NAME <- 'H3K9me3_HCT116.ENCFF832IOO.hg19'
 
 ###
 
@@ -22,14 +22,15 @@ ggsave(paste0('filter_peaks.', NAME, '.init.hist.pdf'), path = OUT_DIR)
 # Remove long peaks
 bed_df <- bed_df %>%
   arrange(-len) %>%
-  filter(len < 5000)
+  filter(len < 2000)
 
 ggplot(bed_df) +
   aes(x = len) +
   geom_histogram() +
   ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
   theme_bw()
-ggsave(paste0('filter_peaks.', NAME, '.filtered.hist.pdf'), path = OUT_DIR)
+#ggsave(paste0('filter_peaks.', NAME, '.filtered.hist.pdf'), path = OUT_DIR)
+ggsave(paste0('filter_peaks.', NAME, '.filtered.hist.png'), path = OUT_DIR)
 
 bed_df %>%
   select(-len) %>%
