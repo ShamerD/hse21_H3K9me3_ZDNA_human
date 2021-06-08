d0402e63453afabb81b2b579243f91e405e9edf0
ShamerD <shomax31@gmail.com>
2021-06-08 20:50:14 +0300

    DeepZ analysis

--- src/chip_seeker.R
+++ src/chip_seeker.R
@@ -18,9 +18,9 @@ library(clusterProfiler)
 ###
 
 #NAME <- 'H3K4me3_A549.intersect_with_DeepZ'
-#NAME <- 'DeepZ'
+NAME <- 'DeepZ'
 #NAME <- 'H3K9me3_HCT116.ENCFF158YTR.hg19.filtered'
-NAME <- 'H3K9me3_HCT116.ENCFF832IOO.hg19.filtered'
+#NAME <- 'H3K9me3_HCT116.ENCFF832IOO.hg19.filtered'
 BED_FN <- paste0(DATA_DIR, NAME, '.bed')
 
 ###
@@ -29,12 +29,14 @@ txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
 
 peakAnno <- annotatePeak(BED_FN, tssRegion=c(-3000, 3000), TxDb=txdb, annoDb="org.Hs.eg.db")
 
-#pdf(paste0(OUT_DIR, 'chip_seeker.', NAME, '.plotAnnoPie.pdf'))
-png(paste0(OUT_DIR, 'chip_seeker.', NAME, '.plotAnnoPie.png'))
+pdf(paste0(OUT_DIR, 'chip_seeker.', NAME, '.plotAnnoPie.pdf'))
+#png(paste0(OUT_DIR, 'chip_seeker.', NAME, '.plotAnnoPie.png'))
 plotAnnoPie(peakAnno)
 dev.off()
 
 peak <- readPeakFile(BED_FN)
 pdf(paste0(OUT_DIR, 'chip_seeker.', NAME, '.covplot.pdf'))
-covplot(peak, weightCol="V5")
+#covplot(peak, weightCol="V5")
+covplot(peak)
 dev.off()
+
